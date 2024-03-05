a0a540ee798b82dd7078a4f2caae401eda1fa0cf
Michaela Arteberry <michaela.arteberry@gmail.com>
2024-03-05 16:40:36 -0500

    Completed

--- monolith/events/api_views.py
+++ monolith/events/api_views.py
@@ -9,7 +9,7 @@ from .models import Conference, Location, State
 
 class LocationListEncoder(ModelEncoder):
     model = Location
-    properties = ["name", "picture_url"]
+    properties = ["name", "picture_url", "id"]
 
 
 class LocationDetailEncoder(ModelEncoder):
@@ -251,3 +251,23 @@ def api_show_location(request, pk):
             encoder=LocationDetailEncoder,
             safe=False,
         )
+
+
+@require_http_methods(["GET"])
+def api_list_states(request):
+    # Get the states from the database ordered by name
+    states = State.objects.order_by("name")
+    # Create an empty list named state_list
+    state_list = []
+    # For each state in the states from the database
+    for state in states:
+        # Create a dictionary that contains the name and
+        # abbreviation for each state
+        state_dict = {
+            "name": state.name,
+            "abbreviation": state.abbreviation,
+        }
+        # Append the dictionary to the list
+        state_list.append(state_dict)
+
+    return JsonResponse({"states": state_list})
